70c231e212d5e5ab62bc266e1c2ce0448bbf5ea5
ycahome <36544526+ycahome@users.noreply.github.com>
2018-02-23 14:25:03 +0200

    Create initial plugin.py

---
+++ plugin.py
@@ -0,0 +1,139 @@
+
+# pp-manager - PythonPlugin Manager
+#
+# Author: ycahome, 2018
+#
+#  version 1.0.0 (2018-02-23): Initial Version
+#
+#
+#
+"""
+<plugin key="PP-MANAGER" name="Python Plugin Manager" author="ycahome" version="1.0.0" externallink="https://www.domoticz.com/forum/viewtopic.php?t=16266">
+    <params>
+        <param field="Address" label="Check My IP URL" width="200px" required="true" default="https://4.ifcfg.me/ip"/>
+        <param field="Mode2" label="Domoticz Plugin" width="200px">
+            <options>
+                <option label="Idle" value="Idle"  default="true" />
+                <option label="SNMP Reader" value="SNMP Reader"/>
+                <option label="NUT UPS" value="NUT UPS"/>
+            </options>
+        </param>
+        <param field="Mode3" label="Notifications" width="75px">
+            <options>
+                <option label="Notify" value="Notify"/>
+                <option label="Disable" value="Disable"  default="true" />
+            </options>
+        </param>
+         <param field="Mode6" label="Debug" width="75px">
+            <options>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal"  default="true" />
+            </options>
+        </param>
+    </params>
+</plugin>
+"""
+
+import Domoticz
+import hmac
+import hashlib
+import time
+import urllib
+import urllib.request
+import urllib.error
+
+#from urllib2 import urlopen
+from datetime import datetime, timedelta
+
+
+class BasePlugin:
+    enabled = False
+    pluginState = "Not Ready"
+    sessionCookie = ""
+    privateKey = b""
+    socketOn = "FALSE"
+
+    def __init__(self):
+        self.debug = False
+        self.error = False
+        self.nextpoll = datetime.now()
+        self.pollinterval = 60  #Time in seconds between two polls
+        return
+
+    def onStart(self):
+
+        Domoticz.Debug("onStart called")
+        self.pollinterval = int(Parameters["Mode1"])  #Time in seconds between two polls
+
+        if Parameters["Mode6"] == 'Debug':
+            self.debug = True
+            Domoticz.Debugging(1)
+            DumpConfigToLog()
+        else:
+            Domoticz.Debugging(0)
+
+
+        Domoticz.Heartbeat(int(Parameters["Mode1"]))
+
+
+    def onStop(self):
+        Domoticz.Log("Plugin is stopping.")
+        Domoticz.Debugging(0)
+
+    def onHeartbeat(self):
+
+        Domoticz.Debug("onHeartbeat called")
+
+
+
+
+
+
+
+global _plugin
+_plugin = BasePlugin()
+
+def onStart():
+    global _plugin
+    _plugin.onStart()
+
+def onStop():
+    global _plugin
+    _plugin.onStop()
+
+def onHeartbeat():
+    global _plugin
+    _plugin.onHeartbeat()
+
+
+# Generic helper functions
+def DumpConfigToLog():
+    for x in Parameters:
+        if Parameters[x] != "":
+            Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
+    Domoticz.Debug("Device count: " + str(len(Devices)))
+    for x in Devices:
+        Domoticz.Debug("Device:           " + str(x) + " - " + str(Devices[x]))
+        Domoticz.Debug("Device ID:       '" + str(Devices[x].ID) + "'")
+        Domoticz.Debug("Device Name:     '" + Devices[x].Name + "'")
+        Domoticz.Debug("Device nValue:    " + str(Devices[x].nValue))
+        Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
+    return
+
+
+#
+# Parse an int and return None if no int is given
+#
+
+def parseIntValue(s):
+
+        try:
+            return int(s)
+        except:
+            return None
+
+
+def mid(s, offset, amount):
+    return s[offset:offset+amount]
+
+
