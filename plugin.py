dc41901aff28f496621739f7b671c7e5d93f3705
ycahome <36544526+ycahome@users.noreply.github.com>
2018-02-23 19:50:05 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -111,7 +111,7 @@ class BasePlugin:
             Domoticz.Error("Folder for Plugin:" + pluginKey + " already exists. Skipping installation!!!")
             Domoticz.Error("Set 'Python Plugin Manager'/ 'Domoticz plugin' attribute to 'idle'.")
         else if pluginText == "Idle":
-            #--
+            Domoticz.Log("Plugin Idle")
         else:
             InstallPythonPlugin(pluginAuthor, pluginRepository, pluginKey)
             
