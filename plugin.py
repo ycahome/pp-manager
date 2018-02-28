ab093572c4debfdb89dbd8674db65016687f9185
ycahome <36544526+ycahome@users.noreply.github.com>
2018-02-28 16:45:33 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -340,7 +340,7 @@ def UpdatePythonPlugin(ppAuthor, ppRepository, ppKey):
             Domoticz.Debug("Git Response:" + str(out))
             if str(out).find("Already up-to-date") != -1:
                Domoticz.Log("Plugin " + ppKey + " already Up-To-Date")
-               #Domoticz.Log("find(error):" + str(str(out).find("error")))
+               Domoticz.Log("find(error):" + str(str(out).find("error")))
             elif (str(out).find("Updating") != -1) and (str(str(out).find("error")) == "-1"):
                Domoticz.Log("Succesfully pulled gitHub update:" + str(out)[str(out).find("Updating")+8:26] + " for plugin " + ppKey)
                Domoticz.Log("---Restarting Domoticz MAY BE REQUIRED to activate new plugins---")
