61ba7b3f05632f616a82855ecae8fb6f033d28fc
ycahome <36544526+ycahome@users.noreply.github.com>
2018-03-13 22:07:54 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -573,7 +573,7 @@ def parseFileForSecurityIssues(pyfilename):
     file = open(pyfilename, "r")
 
     ips = {}
-    safeStrings = ['http://schemas.xmlsoap.org/soap/envelope/','http://schemas.xmlsoap.org/soap/encoding/']
+    safeStrings = ['http://schemas.xmlsoap.org/soap/envelope/','http://schemas.xmlsoap.org/soap/encoding/','import time']
     lineNum = 0
     for text in file.readlines():
        text = text.rstrip()
