84d9368ebf512a0375b20631db227d7e151288b4
ycahome <36544526+ycahome@users.noreply.github.com>
2018-03-13 22:23:04 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -584,12 +584,12 @@ def parseFileForSecurityIssues(pyfilename):
        #Domoticz.Log("'text' is:'" + str(text))
        regexFound = re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})',text)
        if regexFound and str(regexFound) not in safeStrings:
-           Domoticz.Error("Security Finding:'" + str(regexFound) + "' LINE: " + str(lineNum) + " FILE:" + pyfilename)
+           Domoticz.Error("Security Finding:" + str(regexFound) + " LINE: " + str(lineNum) + " FILE:" + pyfilename)
            ips["IP" + str(lineNum)] = (regexFound, "IP Address")
 
        regexFound = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
        if regexFound and str(regexFound) not in safeStrings:
-           Domoticz.Error("Security Finding:'" + str(regexFound) + "' LINE:" + str(lineNum) + " FILE:" + pyfilename)
+           Domoticz.Error("Security Finding:" + str(regexFound) + " LINE:" + str(lineNum) + " FILE:" + pyfilename)
            ips["HTTP" + str(lineNum)] = (regexFound, "HTTP Access")
 
        #regexFound = re.findall('import',text)
@@ -599,7 +599,7 @@ def parseFileForSecurityIssues(pyfilename):
 
        regexFound = re.findall('subprocess.Popen',text)
        if regexFound and str(regexFound) not in safeStrings:
-           Domoticz.Error("Security Finding:'" + str(regexFound) + "' LINE:" + str(lineNum) + " FILE:" + pyfilename)
+           Domoticz.Error("Security Finding:" + str(regexFound) + " LINE:" + str(lineNum) + " FILE:" + pyfilename)
            ips["SUB" + str(lineNum)] = (regexFound, "Subprocess")
 
        lineNum = lineNum + 1
