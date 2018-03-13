99d5f77639dc584e10b00592d2a3b82770d3bf04
ycahome <36544526+ycahome@users.noreply.github.com>
2018-03-13 23:47:39 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -589,7 +589,7 @@ def parseFileForSecurityIssues(pyfilename):
                 ips["IP" + str(lineNum)] = (regexFound[rex], "IP Address")
 
        #regexFound = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
-       regexFound = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]', text)
+       regexFound = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),])', text)
        if regexFound and str(regexFound) not in safeStrings:
             for rex in range(0,len(regexFound)):
                 Domoticz.Error("Security Finding:" + str(regexFound[rex]) + " LINE:" + str(lineNum) + " FILE:" + pyfilename)
