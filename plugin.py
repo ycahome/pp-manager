9e761857c17272ac8c252b668975fefd36f4ce83
ycahome <36544526+ycahome@users.noreply.github.com>
2018-03-14 17:28:45 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -218,7 +218,7 @@ class BasePlugin:
                     Domoticz.Log("SecPolUserList exception (" + secpoluserSection.strip() + "):'" + line.strip() + "'")
                     #SecPolUserList.append(line.strip())
                     #SecPolUserList[secpoluserSection].append(line.strip())
-                    self.SecPolUserList.update({secpoluserSection:line.strip()})
+                    self.SecPolUserList.update({secpoluserSection.strip():line.strip()})
                 # use realine() to read next line
                 line = secpoluserFileHandle.readline()
             secpoluserFileHandle.close()
