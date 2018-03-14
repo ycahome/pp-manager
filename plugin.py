05b8934d2fefe9f5f734eeee30db38141341514d
ycahome <36544526+ycahome@users.noreply.github.com>
2018-03-14 15:17:04 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -623,7 +623,7 @@ def parseFileForSecurityIssues(pyfilename):
            #regexFound[rex] = regexFound[rex].strip('"]')
            Domoticz.Error("Security Finding(IPregex):" + str(regexFound) + " LINE: " + str(lineNum) + " FILE:" + pyfilename)
            for rex in range(0,len(regexFound)):
-                if ((str(text) not in safeStrings) and (str(text).strip() != "")):
+                if ((str(text) not in safeStrings) and (str(text).strip() != "") and (mid(text,0,6) != "<param")):
                     Domoticz.Error("Security Finding(IPtext):" + str(text) + " LINE: " + str(lineNum) + " FILE:" + pyfilename)
                     Domoticz.Error("Security Finding(IPr):" + regexFound[rex] + " LINE: " + str(lineNum) + " FILE:" + pyfilename)
                     ips["IP" + str(lineNum)] = (regexFound[rex], "IP Address")
