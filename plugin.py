a623cf291ee2054f48dfdefcb49ee00c75cb5cd9
ycahome <36544526+ycahome@users.noreply.github.com>
2019-08-03 15:58:47 +0300

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -9,9 +9,9 @@
 
 
 """
-<plugin key="PP-MANAGER" name="Python Plugin Manager" author="ycahome" version="1.5.19" externallink="https://www.domoticz.com/forum/viewtopic.php?f=65&t=22339">
+<plugin key="PP-MANAGER" name="Python Plugin Manager" author="ycahome" version="1.5.20" externallink="https://www.domoticz.com/forum/viewtopic.php?f=65&t=22339">
     <description>
-		<h2>Python Plugin Manager v.1.5.19</h2><br/>
+		<h2>Python Plugin Manager v.1.5.20</h2><br/>
 		<h3>Features</h3>
 		<ul style="list-style-type:square">
 			<li>Install plugins</li>
