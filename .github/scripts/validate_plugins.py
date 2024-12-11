f2d17216e7866751a2f504e4a1b9595ac7210f42
HNO Mobile <heinis.mark@gmail.com>
2024-12-11 14:06:25 +0100

    Update validate_plugins.py

--- .github/scripts/validate_plugins.py
+++ .github/scripts/validate_plugins.py
@@ -1,5 +1,10 @@
 import os
+import sys
 import requests
+
+# Add the repository root to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from plugin import BasePlugin
 
 def validate_repositories():
